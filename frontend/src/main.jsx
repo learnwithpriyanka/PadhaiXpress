5c73c100aa4ebaa494ee0c3abe9405fcacc0fe4b
Priyanka <priyankakumari80921@gmail.com>
2025-03-28 19:31:45 +0530

    aaaaa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import HomePage from './component/HomePage/HomePage.jsx'
+import AboutPage from './component/About/AboutPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HomePage/>
+    <AboutPage/>
   </StrictMode>,
 )
