e15d93ddb6b6a04203e176c96ebd44e48a1fb953
Priyanka <priyankakumari80921@gmail.com>
2025-08-29 08:58:28 +0530

    to switch update pdf

--- frontend/src/showpages/notes/FirstYearNotes.jsx
+++ frontend/src/showpages/notes/FirstYearNotes.jsx
@@ -4,49 +4,29 @@ import { renderYearNotes } from './renderYearNotes';
 export default function FirstYearNotes(){
     const sections = [
         {
-            title: 'Print statement and Java Syntax',
-            description: 'Practice the basic concepts of Java, one of the most widely used object-oriented programming languages.',
+            title: 'c programming',
+            description: 'prepare yourself for c programming languages.',
             items: [
-                { name: 'Print Output in Java' },
-                { name: 'Print Hello World' },
-                { name: 'Identify Correct Syntax' },
-                { name: 'Print difference of 10 and 3' },
-                { name: 'Java Import Syntax' },
-                { name: 'Print String num' }
+                { name: 'Co1 & Co2 pdf notes' },
+                { name: 'Co3 & Co4 pdf notes' },
+                { name: 'importent question' },
+                { name: 'practical question' }
+                
             ]
         },
         {
-            title: 'Variables and Data Types',
-            description: 'Practice problems on variables and different data types.',
+            title: 'Vadic Maths',
+            description: 'prepare yourself for vedic maths.',
             items: [
-                { name: 'Declare and Initialize' },
-                { name: 'Type Conversion' }
+                { name: 'Co1 & Co2 pdf notes' },
+                { name: 'Co3 & Co4 pdf notes' },
+                { name: 'importent question' },
+                { name: 'practical question' }
+                
             ]
         },
-        {
-            title: 'Strings',
-            description: 'Practice basic concepts of strings in Java',
-            items: [
-                { name: 'String Concatenation' },
-                { name: 'Substring and Index' }
-            ]
-        },
-        {
-            title: 'User Input',
-            description: 'Practice problems related to taking input from the user and building custom programs.',
-            items: [
-                { name: 'Scanner Basics' },
-                { name: 'Read Integers and Strings' }
-            ]
-        },
-        {
-            title: 'Algorithmic problems - 1',
-            description: 'Practice simple problems on input, output and basic math',
-            items: [
-                { name: 'Sum and Average' },
-                { name: 'Min/Max of Three' }
-            ]
-        }
+        
+        
     ];
 
     return (
